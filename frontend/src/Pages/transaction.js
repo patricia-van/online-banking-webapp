da59e85973ea8fbc036f6a3c3a9d8941950e5b46
pvsantoso <psantos@ampol.com.sg>
2024-12-20 16:23:15 +0700

    feat(transaction): add new transaction form

--- frontend/src/Pages/transaction.js
+++ frontend/src/Pages/transaction.js
@@ -1,7 +1,14 @@
-import React, { useEffect, useState} from "react";
+import { formDataToBlob } from "formdata-polyfill/esm.min";
+import React, { use, useEffect, useState} from "react";
 
 const ManageTransactions = () => {
     const [transactions, setTransactions] = useState([]);
+    const [showForm, setShowForm] = useState(false)
+    const [formData, setFormData] = useState({
+        date:'',
+        amount:0,
+        comment:''
+    })
 
     useEffect(() => {
         //fetch data for user transactions
@@ -15,12 +22,32 @@ const ManageTransactions = () => {
     }, []);
 
     const handleCreateTransaction = () => {
-        alert('New transaction scheduled')
+        setShowForm(true)
     }
 
     const handleDeleteTransaction = () => [
         alert('Transaction deleted')
     ]
+
+    const handleFormChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const handleNewTransactionSubmit = (e) => {
+        e.preventDefault()
+        const newTransaction = {
+            data: formData.date,
+            amount: parseFloat(formData.amount),
+            comment: formData.comment
+        }
+        setShowForm(false)
+        setFormData({ date:'', amount: 0, description:'' });
+        alert('New transaction scheduled')
+    }
 
 
     return (
@@ -28,9 +55,35 @@ const ManageTransactions = () => {
             <h1>My Transactions</h1>
 
             <div className="button-container">
-                <button className="create-button" onClick={handleCreateTransaction}>New Transactions</button>
-                <button className="delete-button" onClick={handleDeleteTransaction}>Delete Transaction</button>
+                <button className="create-button" onClick={handleCreateTransaction}>
+                    New Transactions
+                </button>
+                <button className="delete-button" onClick={handleDeleteTransaction}>
+                    Delete Transaction
+                </button>
             </div>
+
+            {showForm && (
+                <div className="popup-newtransaction-form">
+                    <h2> Schedule New Transaction </h2>
+                    <form onSubmit={handleNewTransactionSubmit}>
+                        <div>
+                            <label>Date:</label>
+                            <input type="date" name="date" value={formData.date} onChange={handleFormChange} required/>
+                        </div>
+                        <div>
+                            <label>Amount:</label>
+                            <input type="number" name="amount" value={formData.amount} onChange={handleFormChange} required/>
+                        </div>
+                        <div>
+                            <label>Comments:</label>
+                            <input type="text" name='comment' value={formData.comment} onChange={handleFormChange}/>
+                        </div>
+                        <button type="submit">Submit</button>
+                        <button type="button" onClick={() => setShowForm(false)}>Cancel</button>
+                    </form>
+                </div>
+            )}
 
             <h2>Scheduled Transactions</h2>
             {transactions.length > 0 ? (
