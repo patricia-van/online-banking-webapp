a545c559a7e1c7f01a2a42e004743fbdcbc39710
pvsantoso <psantos@ampol.com.sg>
2024-12-30 15:44:16 +0700

    chore(login): remove input boxes and test

--- frontend/src/Pages/login.js
+++ frontend/src/Pages/login.js
@@ -1,49 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
 import { useAuth } from '../AuthProvider'
 import NavBar from '../NavBar'
 
-const Login = (props) => {
-    // const [email, setEmail] = useState('')
-    // const [password, setPassword] = useState('')
-    // const [emailError, setEmailError] = useState('')
-    // const [passwordError, setPasswordError] = useState('')
+const Login = () => {
     const [input, setInput] = useState({
         username: '',
         password: ''
     })
+
     const auth = useAuth()
-    const navigate = useNavigate()
-
-    // const onButtonClick = () => {
-        // // Set initial error values to empty
-        // setEmailError('')
-        // setPasswordError('')
-
-        // // Check if the user has entered both fields correctly
-        // if ('' === email) {
-        //     setEmailError('Please enter your email')
-        //     return
-        // }
-
-        // if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-        //     setEmailError('Please enter a valid email')
-        //     return
-        // }
-
-        // if ('' === password) {
-        //     setPasswordError('Please enter a password')
-        //     return
-        // }
-
-        // if (password.length < 7) {
-        //     setPasswordError('The password must be 8 characters or longer')
-        //     return
-        // }
-
-        // Simulate successful login and naviagte to dashboard
-    //     navigate('/dashboard')
-    // }
 
     const handleSubmitEvent = (e) => {
         e.preventDefault()
@@ -62,68 +27,12 @@ const Login = (props) => {
         }))
     }
 
-    const onTransactionClick = () => {
-        navigate('/transactions')
-    }
-
-    // const [data, setdata] = useState({
-    //     name: "", 
-    //     age: 0,
-    //     data: "",
-    //     programming: ""
-    //   })
-    
-    // useEffect(() => {
-    //     fetch("/data").then((res) => 
-    //       res.json().then((data) => {
-    //         setdata({
-    //           name: data.Name,
-    //           age: data.Age,
-    //           data: data.Date,
-    //           programming: data.programming
-    //         })
-    //       }))
-    //   }, [])
-
-    // const [accounts, setAccounts] = useState([]);
-    
-    // useEffect(() => {
-    //     fetch("/api/accounts").then((res) => {
-    //         res.json().then((data) => {
-    //             if (Array.isArray(data)) {
-    //                 setAccounts(data)
-    //             } else {
-    //                 console.error('Received data not an array')
-    //             }
-                    
-    //         })
-    //     //     if (!res.ok) {
-    //     //         throw new Error('Network response was not ok')
-    //     //     }
-    //     //     return res.json();
-    //     // }).then((data) => {
-    //     //     setAccounts(data)
-    //     // }).catch((error) => {
-    //     //     console.error('There was a problem with the fecth operation')
-    //     })
-    // }, [])
-
     return (
         <div className={'mainContainer'}>
             <div className={'titleContainer'}>
                 <div>Login</div>
                 <p> Welcome </p>
-                {/* <ul>
-                    {accounts.map((account) => (
-                        <li key={account.AccountID}>
-                            AccountID: {account.AccountID}
-                            <br></br>
-                            Balance: {account.AccountBalance}
-                        </li>
-                    ))}
-                </ul> */}
-            </div>
-            <br />
+            </div><br />
 
             <div className={'inputContainer'}>
                 <form onSubmit={handleSubmitEvent}>
@@ -139,33 +48,7 @@ const Login = (props) => {
 
                     <button className='submitButtton'>Submit</button>
                 </form>
-
-                {/* <input
-                    value={email}
-                    placeholder='Enter your email here'
-                    onChange={(ev) => setEmail(ev.target.value)}
-                    className={'inputBox'}
-                />
-                <label className='errorLabel'>{emailError}</label>
             </div>
-            <br />
-
-            <div className={'inputContainer'}>
-                <input
-                    value={password}
-                    placeholder='Enter your password here'
-                    onChange={(ev) => setPassword(ev.target.value)}
-                    className={'inputBox'}
-                />
-                <label className='errorLabel'>{passwordError}</label> */}
-            </div>
-            <br />
-
-            {/* <div className={'inputContainer'}>
-                <input className={'inputButton'} type='button' onClick={onButtonClick} value={'Login'} />
-                <input className={'transactionsButton'} type='button' onClick={onTransactionClick} value={'Manage my Transactions'} />
-            </div> */}
-
         </div>  
     )
 }
